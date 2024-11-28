679949cce7552e10dcc51b1f2b102149f8ba97da
JesusMoraG23 <109312353+JesusMoraG23@users.noreply.github.com>
2024-11-27 20:29:59 -0600

    Update principal-relojes-caballeros.php

--- api/principal-relojes-caballeros.php
+++ api/principal-relojes-caballeros.php
@@ -18,7 +18,7 @@
 		/>
 		<!-- PARA EL CARRITO-->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-		<link rel="stylesheet" href="carrito.css">
+		<link rel="stylesheet" href="carrito-style.css">
 		<script src="carrito.js" async></script>
 	</head>
 	<body>
